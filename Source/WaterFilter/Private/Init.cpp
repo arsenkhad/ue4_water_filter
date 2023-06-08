7b09337c9c8ca2501f438b05fcacbf13fad72321
arsenkhad <arsenkhad@gmail.com>
2023-06-08 11:02:15 +0300

    Removed water generation. Added attempt to load XML

--- Source/WaterFilter/Private/Init.cpp
+++ Source/WaterFilter/Private/Init.cpp
@@ -10,11 +10,8 @@ void AInit::BeginPlay()
 {
 	Super::BeginPlay();
 	FilterReady = false;
-}
-
-void AInit::OnConstruction(const FTransform& Transform)
-{
-	Super::OnConstruction(Transform);
+	ParseXML("D:/'Unreal Engine'/Unreal_Projects/WaterFilter/Config/config.xml");
+	
 	if (Filter == nullptr)
 	{
 		Filter = GetWorld()->SpawnActor<AFilterCylinder>(AFilterCylinder::StaticClass(), FTransform(), SpawnParameters);
@@ -24,6 +21,7 @@ void AInit::OnConstruction(const FTransform& Transform)
 
 	if (!FilterBalls.Num())
 		GenerateCloud(FilterBalls, CylinderH * 8, OuterCylinderD * 2, SphereDiameter, SphereDiameterTolerance);
+
 }
 
 void AInit::Destroyed()
@@ -65,14 +63,31 @@ void AInit::Tick(float DeltaTime)
 		}
 		Filter->Destroy();
 		FilterReady = true;
-		ABall* WaterBall = NewObject<ABall>(this, ABall::StaticClass());
-		return;
 	}
-
-	if (FilterReady && GetWorld()->GetFirstPlayerController()->WasInputKeyJustPressed(EKeys::SpaceBar))
-		GenerateCloud(WaterBalls, ParticlesH, OuterCylinderD, WaterParticleDiameter);
 }
 
-// ABall* Ball = NewObject<ABall>(this, ABall::StaticClass());
-// Ball->SetActorRelativeTransform(TransformSetting);
-// FilterBalls.Emplace(Ball);
+void AInit::ParseXML(const FString& Path)
+{
+	FXmlFile* File = new FXmlFile(Path, EConstructMethod::ConstructFromFile);
+	const FXmlNode* RootNode = File->GetRootNode();
+	if (RootNode == nullptr)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("XML file open failed. Using defaults.") );
+	}
+	else
+	{
+		TMap<FString, float*> Attributes;
+		Attributes.Add("SphereDiameter", &SphereDiameter);
+		Attributes.Add("SphereDiameterTolerance", &SphereDiameterTolerance);
+		Attributes.Add("InnerCylinderD", &InnerCylinderD);
+		Attributes.Add("OuterCylinderD", &OuterCylinderD);
+		Attributes.Add("CylinderH", &CylinderH);
+		Attributes.Add("CylindersDelta", &CylindersDelta);
+		for(const auto &Attribute : RootNode->GetAttributes())
+		{
+			FString ValueString = Attribute.GetValue();
+			float Value = FCString::Atof(*ValueString);
+			Attributes[Attribute.GetTag()] = &Value;
+		}
+	}
+}
--- Source/WaterFilter/WaterFilter.Build.cs
+++ Source/WaterFilter/WaterFilter.Build.cs
@@ -8,7 +8,7 @@ public class WaterFilter : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "ProceduralMeshComponent" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "ProceduralMeshComponent", "XmlParser" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
