e6689fced21662ffbebe99d9840dd32a2cfc174c
arsenkhad <arsenkhad@gmail.com>
2023-06-08 11:02:15 +0300

    Removed water generation. Added attempt to load XML

--- Source/WaterFilter/Public/Init.h
+++ Source/WaterFilter/Public/Init.h
@@ -5,6 +5,7 @@
 #include "CoreMinimal.h"
 #include "Ball.h"
 #include "FilterCylinder.h"
+#include "XmlFile.h"
 #include "GameFramework/Actor.h"
 #include "Init.generated.h"
 
@@ -16,6 +17,8 @@ class WATERFILTER_API AInit : public AActor
 public:	
 	AInit();
 	virtual void Tick(float DeltaTime) override;
+
+	void ParseXML(const FString& Path);
 	UPROPERTY(EditAnywhere)
     float SphereDiameter = 100.f;
     UPROPERTY(EditAnywhere)
@@ -28,25 +31,18 @@ public:
     float CylinderH = 800.f;
     UPROPERTY(EditAnywhere)
     float CylindersDelta = 200.f;
-	UPROPERTY(EditAnywhere)
-	float ScaleFactor = 1000000.f;
-    UPROPERTY(EditAnywhere)
-    float ParticlesH = 2.f;
 	
 protected:
 	virtual void BeginPlay() override;
-	virtual void OnConstruction(const FTransform& Transform) override;
 	virtual void Destroyed() override;
 	
 	float DiameterRandomize(float OffsetD) const {return OffsetD ? FMath::RandRange(-OffsetD, OffsetD) : 0;}
 	void GenerateCloud(TArray<ABall*>& Cloud, const float CloudHeight, const float CloudSize, const float BallDiameter, const float OffsetD) const;
 	
-	// float ScaleFactor = FMath::RandRange(static_cast<int64>(pow(10, 6)), pow(10, 9)); 
-	float WaterParticleDiameter = SphereDiameter / ScaleFactor;
 	bool FilterReady = false;
 	
 	UPROPERTY(VisibleAnywhere)
-	AFilterCylinder* Filter;
+	AFilterCylinder* Filter = nullptr;
 	UPROPERTY(VisibleAnywhere)
 	TArray<ABall*> FilterBalls;
 	UPROPERTY(VisibleAnywhere)
